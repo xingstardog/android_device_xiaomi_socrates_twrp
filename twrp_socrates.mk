b572370788b40bc4d25014d47c22d2e7de4f6f7e
xingstardog <zyh20120515@vip.qq.com>
2026-08-23 23:13:26 +0800

    Convert CRLF to LF for linux compatibility

--- twrp_socrates.mk
+++ twrp_socrates.mk
@@ -1,4 +1,4 @@
-﻿# Inherit from those products. Most specific first.
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
